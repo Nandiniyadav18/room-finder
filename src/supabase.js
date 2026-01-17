8e3bab604b977e0e2926dcb1d477c194bd182de9
Nandiniyadav18 <nandiniit14@gmail.com>
2026-01-17 16:05:53 +0530

    Fix Supabase env variable usage

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from "@supabase/supabase-js"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = import.meta.env.https://hztwiucwdizxonzoketx.supabase.co
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6dHdpdWN3ZGl6eG9uem9rZXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODkzODgsImV4cCI6MjA4Mzk2NTM4OH0.jCgg0jDjqm627-5lEWyCWIzV2pz4L-QiRbYuC9VGdmU
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
