70ff04c78e2f9c68bc0297f22b6345cd297cb92b
Nandiniyadav18 <nandiniit14@gmail.com>
2026-01-17 15:32:11 +0530

    Configure Supabase client with env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://hztwiucwdizxonzoketx.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6dHdpdWN3ZGl6eG9uem9rZXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODkzODgsImV4cCI6MjA4Mzk2NTM4OH0.jCgg0jDjqm627-5lEWyCWIzV2pz4L-QiRbYuC9VGdmU"
+const supabaseUrl = import.meta.env.https://hztwiucwdizxonzoketx.supabase.co
+const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6dHdpdWN3ZGl6eG9uem9rZXR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzODkzODgsImV4cCI6MjA4Mzk2NTM4OH0.jCgg0jDjqm627-5lEWyCWIzV2pz4L-QiRbYuC9VGdmU
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
